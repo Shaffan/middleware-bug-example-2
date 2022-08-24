d4910ce6bac53d9f1c6dacac847339bc121c4895
Stefan Horne <hello@stefanhorne.com>
2022-08-24 11:16:59 +0100

    testing

--- middleware.js
+++ middleware.js
@@ -1,6 +1,10 @@
 const { NextResponse } = require("next/server");
 
+const { SOME_ENV_VARIABLE = 0 } = process.env
+
 export function middleware(req) {
+  if (SOME_ENV_VARIABLE) return NextResponse.next()
+
   console.log("hit");
   console.log(req.nextUrl);
 
