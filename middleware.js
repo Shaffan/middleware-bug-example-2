37b05cfc4d4bd879e367a703a8dfba3ded064de4
Stefan Horne <hello@stefanhorne.com>
2022-08-24 11:13:45 +0100

    remove matcher

--- middleware.js
+++ middleware.js
@@ -6,10 +6,3 @@ export function middleware(req) {
 
   return NextResponse.next();
 }
-
-export const config = {
-  matcher: [
-    // eslint-disable-next-line prettier/prettier, no-useless-escape
-    "/:path((?!c/|_next/?|o/|f/|location/?|rating/?|price/?|product/?|basket/?|things-to-do/?|experience-blog/?|sapling/?).*(?<!\\..*))"
-  ]
-};
